c73d4d8538dc2c8c3a36d63c355fabb68fb10d48
João Rosinhole <jvr131104@gmail.com>
2024-07-22 12:30:25 -0300

    search-bar and home finished

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import mockData from "../../mocks/cursos-mock.json"
+import { CommonModule } from '@angular/common';
+import { MyFilterPipe } from '../../pipes/filter.pipe';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    MyFilterPipe
+  ],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss'
 })
+
 export class SearchBarComponent {
+  public cursos = mockData.cursos;
+
   search = new FormControl('');
 }
