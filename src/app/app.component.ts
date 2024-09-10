fca6e23fa9298126dc01e1b939511df48bfe2384
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-10 20:29:44 -0300

    teste-de-nivelamento começado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { SearchBarComponent } from "./components/search-bar/search-bar.component";
+import { TesteDeNivelamentoComponent } from './components/teste-de-nivelamento/teste-de-nivelamento.component';
 import { CommonModule, Location } from '@angular/common';
 
 @Component({
@@ -11,12 +12,14 @@ import { CommonModule, Location } from '@angular/common';
     imports: [
       RouterOutlet,
       SearchBarComponent,
+      TesteDeNivelamentoComponent,
       CommonModule
     ]
 })
 export class AppComponent {
   public userName = 'André';
   public page = '';
+  public nivelUser = '';
 
   constructor(private location: Location, private router: Router) {
     //Essa função verifica a url e qualquer mudança que ocorre nela. Ele faz isso para mudar o estilo dos botões da navbar
---
+++ src/app/components/teste-de-nivelamento/teste-de-nivelamento.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teste-de-nivelamento',
+  standalone: true,
+  imports: [],
+  templateUrl: './teste-de-nivelamento.component.html',
+  styleUrl: './teste-de-nivelamento.component.scss'
+})
+export class TesteDeNivelamentoComponent {
+
+}
