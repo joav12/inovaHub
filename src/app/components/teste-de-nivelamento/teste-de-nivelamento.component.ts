78a6fcf5c5ffd2a83045f460feb2441453fa77b8
João Rosinhole <jvr131104@gmail.com>
2024-09-12 16:57:10 -0300

    teste de nivelamente quase completo

--- src/app/components/teste-de-nivelamento/teste-de-nivelamento.component.ts
+++ src/app/components/teste-de-nivelamento/teste-de-nivelamento.component.ts
@@ -18,13 +18,34 @@ export class TesteDeNivelamentoComponent {
   public testeText = 'Seja muito bem-vindo(a) a <b>InovaHub</b> e ao teste de nivelamento! <br><br>Neste teste, vamos explorar diferentes abordagens e identificar os roteiros que melhor se alinham com suas necessidades e objetivos educacionais. Nosso objetivo é proporcionar uma experiência de aprendizado personalizada e eficaz.<br><br>Para começar esta jornada de descoberta, clique no botão abaixo e vamos começar!';
   public bvdOrResul: boolean = true
   public questaoAtual = 1;
+  public btnCont = true;
+  public respostasSalvas: Array<any> = [];
+  public guardaResp: any;
 
   public perguntasTDN = mockData.perguntasTDN;
 
   constructor(){ }
 
+  selecionaResposta(questao: any, resposta: any){
+    this.btnCont = false;
+    this.guardaResp = {'questao': questao, 'respostaEscolhida': resposta}
+
+    console.log(resposta);
+  }
+
+  continuaFinalizaTeste(respostasDaQuestao: any){
+    this.respostasSalvas.push(this.guardaResp);
+
+    if(this.perguntasTDN[this.questaoAtual]){
+      this.questaoAtual++
+      this.btnCont = true;
+    }
+    else{
+      console.log('batata')
+    }
+  }
+
   vaParaQuestoes(){
     this.bvdOrResul = false;
-
   }
 }
