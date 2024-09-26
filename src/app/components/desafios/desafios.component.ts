f4cae190fe3991d023a996791c07a69d98d5d4e3
João Rosinhole <jvr131104@gmail.com>
2024-09-26 16:42:08 -0300

    desafios feito

--- src/app/components/desafios/desafios.component.ts
+++ src/app/components/desafios/desafios.component.ts
@@ -16,6 +16,9 @@ export class DesafiosComponent {
   public cursosFinalizados: number = 0;
   public cursosComecados: number = 0;
   public aulasFeitasNum: number = 0;
+  public tempoDiario: number = Date.now() - Date.parse('December 31 2015 5:59:59');;
+  public tempoSemanal: number = Date.now();
+  public diaNumero: number = 0;
 
   private verificaCursosEAulas(): void{
     this,this.cursosFinalizados = Number(sessionStorage.getItem('cursosFinalizados'));
@@ -36,7 +39,33 @@ export class DesafiosComponent {
     }
   }
 
+  public diaDaSemana(dia: any): number{
+    if(dia == 'Monday'){
+      return this.diaNumero = 6;
+    }
+    else if(dia == 'Tuesday'){
+      return this.diaNumero = 5;
+    }
+    else if(dia == 'Wednesday'){
+      return this.diaNumero = 4;
+    }
+    else if(dia == 'Thursday'){
+      return this.diaNumero = 3;
+    }
+    else if(dia == 'Friday'){
+      return this.diaNumero = 2;
+    }
+    else if(dia == 'Saturday'){
+      return this.diaNumero = 1;
+    }
+    else if(dia == 'Sunday'){
+      return this.diaNumero = 0;
+    }
+    return 0;
+  }
+
   ngOnInit(): void{
     this.verificaCursosEAulas();
+    // this.tempoDiario = this.tempoDiario - Date.parse('December 31 2015 5:59:59');
   }
 }
